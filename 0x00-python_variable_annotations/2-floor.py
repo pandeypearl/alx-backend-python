9c17529142857093527cdd52a6c25c96ae91b422
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-06 01:42:49 -0700

    Task 2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Type-annotation floor funtion """
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Acceps a float as argument  and returns the floor of float
+    """
+    return math.floor(n)
