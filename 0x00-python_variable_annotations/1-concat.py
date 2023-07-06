7e0fe6e5c2ded3a9e34ab1e856b73c7ca3a39d89
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-06 03:46:03 -0700

    Making file pycodestyle compliant

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Annotated contatinated string function """
 
+
 def concat(str1: str, str2: str) -> str:
     """ Accepts two string arguments and returns their
     concatination string """
