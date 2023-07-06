d83d1d1a4b46f88171321371f99a6ffe0eeb40d6
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-06 01:57:30 -0700

    Task 4. Define variables

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Type-annotation to_str funtion """
+
+
+def to_str(n: float) -> str:
+    """
+    Accepts float as argument and returns
+    string representation
+    """
+    return str(n)
