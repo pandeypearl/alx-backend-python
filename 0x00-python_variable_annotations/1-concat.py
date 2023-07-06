30912bcea162542c2c7abe31112a241cc90b9857
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-06 01:23:46 -0700

    Task 1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Annotated contatinated string function """
+
+def concat(str1: str, str2: str) -> str:
+    """ Accepts two string arguments and returns their
+    concatination string """
+    return str1 + str2
