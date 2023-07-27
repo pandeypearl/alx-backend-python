7f95d3c62e31d5074ad5d757df02fb5a2b5f0367
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-27 09:37:44 +0000

    Task 5. Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -28,3 +28,15 @@ class TestGithubOrgClient(unittest.TestCase):
         spec = GithubOrgClient(data)
         spec.org()
         mock.assert_called_once_with(endpoint)
+
+    @parameterized.expand([
+        ('random_url', {'repos_url': 'http//some_url.com'})
+    ])
+    def test_public_repos_url(self, name, result):
+        """
+        Self Descriptive
+        """
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=result)):
+            response = GithubOrgClient(name)._public_repos_url
+            self.assertEqual(response, result.get('repos_url'))
